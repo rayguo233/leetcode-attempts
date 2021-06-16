672def2dc25d9870fcff47297c072d28145a1e6a
Ray Guo <siruiguo@outlook.com>
2021-06-16 19:50:59 +0800

    added 33. [M] rotated sorted array

---
+++ 33. [M] Search in Rotated Sorted Array.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+# There is an integer array nums sorted in ascending order
+# (with distinct values).
+#
+# Prior to being passed to your function, nums is rotated at
+# an unknown pivot index k (0 <= k < nums.length) such that
+# the resulting array is [nums[k], nums[k+1], ..., nums[n-1],
+# nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example,
+# [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become
+# [4,5,6,7,0,1,2].
+#
+# Given the array nums after the rotation and an integer
+# target, return the index of target if it is in nums, or
+# -1 if it is not in nums.
+#
+# You must write an algorithm with O(log n) runtime complexity.
+
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    # print(sol.invertTree([2,1,4]))
+    # assert(sol.invertTree([1,2,1,2]) == 1)
