0b81818192b8a6148eb8a3b2678e5e6df5a45367
Ray Guo <siruiguo@outlook.com>
2021-06-16 22:51:51 +0800

    solved 33. [M] rotated sorted array

--- 33. [M] Search in Rotated Sorted Array.py
+++ 33. [M] Search in Rotated Sorted Array.py
@@ -14,14 +14,41 @@
 # target, return the index of target if it is in nums, or
 # -1 if it is not in nums.
 #
+# 1 <= nums.length
+#
 # You must write an algorithm with O(log n) runtime complexity.
 
 class Solution:
     def search(self, nums: list[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        if nums[-1] == target:
+            return r
+        while l < r:
+            m = int((l + r) / 2)
+            if nums[m] == target:
+                return m
+            # if mid and target on the same side
+            if ((nums[m] - nums[-1]) * (target - nums[-1])) >= 0:
+                if target < nums[m]:
+                    r = m
+                else:
+                    l = m + 1
+            # else mid and target on different side
+            else:
+                if target < nums[m]:
+                    l = m + 1
+                else:  # target > nums[m]
+                    r = m
+        assert (l == r)
+        return l if nums[l] == target else -1
+
+
 
 
 
 if __name__ == '__main__':
     sol = Solution()
-    # print(sol.invertTree([2,1,4]))
+    # print(sol.search([4,5,6,7,0,1,2], 3))
+    # print(sol.search([4, 5], 4))
+    print(sol.search([1,3,5], 1))
     # assert(sol.invertTree([1,2,1,2]) == 1)
