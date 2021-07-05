a499c84764f74d6378d7e630def6bfb42432dc2b
rayguo <siruiguo@outlook.com>
2021-07-05 22:26:02 +0800

    fixed quick sort

--- 912. [M] Sort an Array.py
+++ 912. [M] Sort an Array.py
@@ -3,28 +3,37 @@
 # YGiven an array of integers nums, sort the array
 # in ascending order.
 
+import random
+
+
 class Solution:
-    def sortArray_quick_sort(self, nums: list[int]) -> list[int]:
-        self.quick_sort(nums, 0, len(nums)-1)
+    def sortArray_qs(self, nums: list[int]) -> list[int]:
+        self.quick_sort(nums, 0, len(nums) - 1)
         return nums
 
     def quick_sort(self, nums, l, r):
         if r - l <= 0: return
-        pvt = self.partition(nums, 0, r)
-        self.quick_sort(nums, 0, pvt - 1)
+        pvt = self.partition(nums, l, r)
+        self.quick_sort(nums, l, pvt - 1)
         self.quick_sort(nums, pvt + 1, r)
 
     def partition(self, nums, l, r) -> int:
+        p = random.randrange(l, r + 1)
+        self.swap(nums, p, r)
         p = r
         pvt = nums[r]
-        while l < r:
-            if nums[l] <= pvt: l += 1
-            elif nums[r] < pvt:
+        r -= 1
+        while l <= r:
+            if nums[l] <= pvt:
+                l += 1
+            elif nums[r] <= pvt:
                 self.swap(nums, l, r)
-                l, r = l+1, r-1
-            else: r -= 1
-        if l < p: self.swap(nums, l, p)
-        return l
+                l, r = l + 1, r - 1
+            else:
+                r -= 1
+        if l < p:
+            self.swap(nums, l, p)
+        return min(l, p)
 
     def swap(self, nums, a, b):
         # print(nums, a, b)
@@ -32,6 +41,7 @@ class Solution:
 
 if __name__ == '__main__':
     sol = Solution()
-    a = [1,2,3,2,5,2,4]
-    sol.sortArray_quick_sort(a)
+    a = [-4,0,7,4,9,-5,-1,0,-7,-1]
+    a = [5,2,3,1]
+    sol.sortArray_qs(a)
     print(a)
