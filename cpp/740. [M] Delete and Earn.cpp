02e9d0e01f1d31d1290409db85dae7cf8c57c8e2
rayguo <siruiguo@outlook.com>
2021-07-10 21:52:14 +0800

    incomplete question

---
+++ cpp/740. [M] Delete and Earn.cpp
@@ -0,0 +1,23 @@
+//
+
+// 1 <= nums.length
+
+#include <vector>  
+#include <math.h>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int deleteAndEarn(vector<int>& nums) {
+        int size = nums.size();
+        
+    }
+};
+
+int main() {
+    Solution sol = Solution();
+    vector<vector<int>> a;
+    a = {{3},{2}};
+}
