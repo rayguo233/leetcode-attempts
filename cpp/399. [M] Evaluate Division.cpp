888c4ce0a869c70e3040cd1a099cce24a9691737
rayguo <siruiguo@outlook.com>
2021-07-18 10:51:27 +0800

    incomplete

---
+++ cpp/399. [M] Evaluate Division.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/two-sum/
+
+#include <vector>
+#include <unordered_map>
+#include <assert.h>
+#include <iostream>
+
+using namespace std;;
+
+class Solution {
+public:
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        
+    }
+private:
+    unordered_map<string, pair<string, int>> parent_;
+};
+
+int main() {
+    Solution sol = Solution();
+}
