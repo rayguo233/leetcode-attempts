3f29580000b3a41b15c3287a39896f44734e89d6
Ray Guo <siruiguo@outlook.com>
2021-07-22 13:15:00 +0800

    easy question

---
+++ cpp/EASY 532. [M] K-diff Pairs in an Array.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <unordered_map>
+#include <assert.h>
+#include <iostream>
+
+using namespace std;;
+
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        int size = nums.size();
+        sort(nums.begin(), nums.end());
+    }
+};
+
+int main() {
+    Solution sol = Solution();
+    vector<int> input = {1,2,3,4,5,6,7};
+}
