172efb0a0f2c692de45d6df40fad95719439bef1
Ray Guo <siruiguo@outlook.com>
2021-08-11 20:52:25 +0800

    1438 M

---
+++ cpp/1438. [M] Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <set>
+
+using namespace std;
+
+class Solution {
+public:
+    int longestSubarray(const vector<int>& nums, int limit) {
+        int res = 1;
+        int l = 0, r = 0;
+        multiset<int> s;
+        while (true) {
+            while (s.empty() || *(s.rbegin()) - *(s.begin()) <= limit) {
+                res = max(res, (int)s.size());
+                if (r == nums.size())
+                    return res;
+                s.insert(nums[r]);
+                r++;
+            }
+            while (!s.empty() && *(s.rbegin()) - *(s.begin()) > limit) {
+                auto itr = s.find(nums[l]);
+                s.erase(itr, ++itr);
+                l++;
+            }
+        }
+        assert(false);
+        return res;
+    }
+};
+
+int main() {
+    [2,2,2,4,4,2,5,5,5,5,5,2]
+2
+}
