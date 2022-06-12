b4ac07cbafaa06d549787081f2e1af05f00507a4
rayguo <siruiguo@outlook.com>
2022-06-12 19:57:35 +0800

    300. [M] Longest Increasing Subsequence

--- python/300. [M] Longest Increasing Subsequence.py
+++ python/300. [M] Longest Increasing Subsequence.py
@@ -12,8 +12,22 @@
 # 链接：https://leetcode-cn.com/problems/longest-increasing-subsequence
 # 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
+import bisect
+from typing import List
+
+
 class Solution:
-    def lengthOfLIS(self, nums: list[int]) -> int:
+
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        len_to_last = [float('inf')] * len(nums)
+        max_len = 0
+        for num in nums:
+            index = bisect.bisect_left(len_to_last, num, hi=max_len)
+            len_to_last[index] = min(num, len_to_last[index])
+            max_len = max(max_len, index + 1)
+        return max_len
+
+    def lengthOfLIS_v0(self, nums: list[int]) -> int:
         if not nums: return 0
         res = 1
         dp = [1] * len(nums)
@@ -25,6 +39,10 @@ class Solution:
             res = max(res, maxi)
         return res
 
+
 if __name__ == '__main__':
     sol = Solution()
-    # print(sol.maxProfit(2, [3,2,6,5,0,3]))
+    print(sol.lengthOfLIS([1, 2, 3, 4]))
+    print(sol.lengthOfLIS([1, 0, 3, 4]))
+    print(sol.lengthOfLIS([4, 0, 3, 2]))
+    print(sol.lengthOfLIS([7, 7, 7, 7, 7, 7, 7]))
