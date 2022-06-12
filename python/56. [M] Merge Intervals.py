705b6952f81ae9a0cf75868b8dfa48b79f2d16b2
rayguo <siruiguo@outlook.com>
2022-06-12 10:09:16 +0800

    56. [M] Merge Intervals

--- python/56. [M] Merge Intervals.py
+++ python/56. [M] Merge Intervals.py
@@ -11,7 +11,24 @@
 # 链接：https://leetcode-cn.com/problems/merge-intervals
 # 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
+from typing import List
+
+
 class Solution:
+
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+        res = []
+        start, end = intervals[0]
+        for curr_start, curr_end in intervals:
+            if curr_start <= end:
+                end = max(end, curr_end)
+                continue
+            res.append(start, end)
+            start, end = curr_start, curr_end
+        res.append(start, end)
+        return res
+
     def merge(self, intervals: list[list[int]]) -> list[list[int]]:
         intervals.sort(key=lambda x: x[0])
         prev = intervals[0]
@@ -43,12 +60,10 @@ class Solution:
         return res
 
 
-
-
 if __name__ == '__main__':
     sol = Solution()
     # print(sol.merge([[1,2]]))
-    print(sol.merge_try2([[1,2],[0,9],[6,7]]))
+    print(sol.merge_try2([[1, 2], [0, 9], [6, 7]]))
     # assert(sol.rob([2,4]) == 4)
     # assert(sol.rob([2,1,4,8]) == 9)
     # assert(sol.rob([2,1,4,8,9]) == 13)
